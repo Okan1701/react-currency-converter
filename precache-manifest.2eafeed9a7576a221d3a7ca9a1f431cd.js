7ebf9fa9ee170f929e37f1d8d0eb1c6027f1d4b0
Okan <okanemeni@outlook.com>
2019-04-15 22:22:47 +0200

    Updates

---
+++ precache-manifest.2eafeed9a7576a221d3a7ca9a1f431cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "44f2f1f13a15ff6e456c",
+    "url": "/react-currency-converter/static/js/runtime~main.2f8f27dc.js"
+  },
+  {
+    "revision": "8b55e4f868409c9e8ff4",
+    "url": "/react-currency-converter/static/js/main.80849a4e.chunk.js"
+  },
+  {
+    "revision": "d2115000794bfca251fd",
+    "url": "/react-currency-converter/static/js/2.5b46e600.chunk.js"
+  },
+  {
+    "revision": "8b55e4f868409c9e8ff4",
+    "url": "/react-currency-converter/static/css/main.d59b34eb.chunk.css"
+  },
+  {
+    "revision": "f85ee99d4b0e76b9f73719e00afd9cf9",
+    "url": "/react-currency-converter/index.html"
+  }
+];
